5de8cda77f964e69d75f863bd19dc93502a84eeb
GOWTHAM1092 <24b11ec092@adityauniversity.in>
2024-12-12 13:51:59 +0530

    Uploading code: Good Turn.c

---
+++ Good Turn.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int t=a+b;
+    if(t>6)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
